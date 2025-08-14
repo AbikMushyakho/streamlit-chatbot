56a247202cfea06dadf802e42a20c5e5b57c1f63
Abik Mushyakho <abik.mushyakho94@gmail.com>
2025-08-14 12:57:46 +1000

    a

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     # embeddings = OpenAIEmbeddings()
-   embeddings = HuggingFaceEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = HuggingFaceEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
